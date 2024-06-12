df82503fe295c19a9df4129495e6ddf638e47706
wayTo <yurigarciadev@gmail.com>
2024-06-12 10:44:13 -0300

    https

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-const express = require("express")
+import express, { json } from "express";
+import fs from "fs";
+import { google } from "googleapis";
 
-const { google } = require("googleapis")
-
-const app = express()
+const app = express();
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Request methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  )
+  );
 
   // Request headers you wish to allow
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,Content-Type,Accept"
-  )
+  );
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", "true")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
   // Pass to next layer of middleware
-  next()
-})
+  next();
+});
 
-app.use(express.json())
+app.use(json());
 
 app.post("/", async (req, res) => {
   const auth = new google.auth.GoogleAuth({
     keyFile: "credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
-  })
+  });
 
-  const client = await auth.getClient()
+  const client = await auth.getClient();
 
-  const googleSheets = google.sheets({ version: "v4", auth: client })
+  const googleSheets = google.sheets({ version: "v4", auth: client });
 
-  const { spreadsheetId, sheetName } = req.body
+  const { spreadsheetId, sheetName } = req.body;
 
   const metaData = await googleSheets.spreadsheets.get({
     auth,
     spreadsheetId,
-  })
+  });
 
   // Read rows
 
@@ -54,11 +54,11 @@ app.post("/", async (req, res) => {
     auth,
     spreadsheetId,
     range: sheetName + "!B:B",
-  })
+  });
 
-  const rowValues = getRows.data.values
-  const rows = rowValues.length
-  const range = sheetName + "!B" + (rows + 1) + ":B9999"
+  const rowValues = getRows.data.values;
+  const rows = rowValues.length;
+  const range = sheetName + "!B" + (rows + 1) + ":B9999";
 
   const {
     entryDate,
@@ -67,7 +67,7 @@ app.post("/", async (req, res) => {
     entryEmail,
     entryPhone,
     entryFGTS,
-  } = req.body
+  } = req.body;
 
   const append = await googleSheets.spreadsheets.values.append({
     auth,
@@ -87,22 +87,22 @@ app.post("/", async (req, res) => {
         ],
       ],
     },
-  })
+  });
 
-  res.send(append.data.values).status(200)
-})
+  res.send(append.data.values).status(200);
+});
 
 app.patch("/", async (req, res) => {
   try {
     const auth = new google.auth.GoogleAuth({
       keyFile: "credentials.json",
       scopes: "https://www.googleapis.com/auth/spreadsheets",
-    })
+    });
 
-    const client = await auth.getClient()
+    const client = await auth.getClient();
 
-    const googleSheets = google.sheets({ version: "v4", auth: client })
-    const { spreadsheetId, sheetName } = req.body
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+    const { spreadsheetId, sheetName } = req.body;
 
     // Read rows
 
@@ -110,25 +110,25 @@ app.patch("/", async (req, res) => {
       auth,
       spreadsheetId,
       range: sheetName,
-    })
+    });
 
-    res.send(getRows) //Rota principal da aplicacao, para acesso direto na port
+    res.send(getRows); //Rota principal da aplicacao, para acesso direto na port
   } catch (error) {
-    res.send(error.message)
+    res.send(error.message);
   }
-})
+});
 
 app.patch("/get-used-rows", async (req, res) => {
   try {
     const auth = new google.auth.GoogleAuth({
       keyFile: "credentials.json",
       scopes: "https://www.googleapis.com/auth/spreadsheets",
-    })
+    });
 
-    const client = await auth.getClient()
+    const client = await auth.getClient();
 
-    const googleSheets = google.sheets({ version: "v4", auth: client })
-    const { spreadsheetId, sheetName } = req.body
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+    const { spreadsheetId, sheetName } = req.body;
 
     // Read rows
 
@@ -136,15 +136,28 @@ app.patch("/get-used-rows", async (req, res) => {
       auth,
       spreadsheetId,
       range: sheetName + "!B:B",
-    })
-    const rowValues = getRows.data.values
-    const rows = rowValues.length
-    res.send({ rows }) //Rota principal da aplicacao, para acesso direto na port
+    });
+    const rowValues = getRows.data.values;
+    const rows = rowValues.length;
+    res.send({ rows }); //Rota principal da aplicacao, para acesso direto na port
   } catch (error) {
-    res.send(error.message)
+    res.send(error.message);
   }
-})
+});
 
-app.listen(21231, (req, res) =>
-  console.log("running on http://localhost:21231")
-)
+const options = {
+  key: fs.readFileSync(
+    "../../../etc/letsencrypt/live/alabarda.link/privkey.pem"
+  ),
+  cert: fs.readFileSync(
+    "../../../etc/letsencrypt/live/alabarda.link/fullchain.pem"
+  ),
+};
+https
+  .createServer(options, app)
+  .listen(21231, () =>
+    console.log(
+      "spreadsheet-editor https server online on 21231 and using node version " +
+        process.version
+    )
+  );
